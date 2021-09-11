fcdb0ca088ad8ddf7e0b0dc98623fd3e250295e3
Matyáš Sládek <acc.sladek@gmail.com>
2021-09-11 03:02:37 +0200

    Added global exception and custom error handling

---
+++ MusicStore/Controllers/ErrorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Error/{statusCode}")]
+        [AllowAnonymous]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            _logger.LogWarning($"[{statusCode}]: {ReasonPhrases.GetReasonPhrase(statusCode).ToUpper()} error occured. Path: \"{statusCodeResult.OriginalPath}\" Query string: \"{statusCodeResult.OriginalQueryString}\"");
+
+            ViewBag.ErrorTitle = $"{statusCode}: {ReasonPhrases.GetReasonPhrase(statusCode)}";
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found.";
+                    break;
+            }
+
+            return View("Error");
+        }
+
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            _logger.LogError($"The path {exceptionDetails.Path} threw an exception {exceptionDetails.Error}");
+
+            return View("Error");
+        }
+    }
+}
--- MusicStore/Startup.cs
+++ MusicStore/Startup.cs
@@ -75,7 +75,9 @@ namespace MusicStore
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
